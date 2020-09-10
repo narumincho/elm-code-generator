839d1c1a6177bcbf36e8e3dead8c08acac9a349f
narumincho <narumincho.starfy@gmail.com>
2020-09-10 09:05:35 +0900

    add Array, Float, Order, Bool, String in util

--- util.ts
+++ util.ts
@@ -1,8 +1,33 @@
 import * as data from "./data";
 import * as main from "./main";
 
+export const Array = data.ElmType.ImportedType({
+  moduleName: "Array",
+  typeName: main.elmTypeNameFromStringOrThrow("Array"),
+});
+
 /** 基本の整数型 JSの出力なら -9007199254740991 ~ 9007199254740991 の範囲に対応している */
 export const Int = data.ElmType.ImportedType({
   moduleName: "Basic",
   typeName: main.elmTypeNameFromStringOrThrow("Int"),
+});
+
+export const Float = data.ElmType.ImportedType({
+  moduleName: "Basic",
+  typeName: main.elmTypeNameFromStringOrThrow("Float"),
+});
+
+export const Order = data.ElmType.ImportedType({
+  moduleName: "Basic",
+  typeName: main.elmTypeNameFromStringOrThrow("Order"),
+});
+
+export const Bool = data.ElmType.ImportedType({
+  moduleName: "Basic",
+  typeName: main.elmTypeNameFromStringOrThrow("Bool"),
+});
+
+export const String = data.ElmType.ImportedType({
+  moduleName: "String",
+  typeName: main.elmTypeNameFromStringOrThrow("String"),
 });
